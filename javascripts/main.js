6211430c59e9a6408d6bd6a9b26a7339c3b2705c
garys-workplace <garys@wpmail.com>
2012-11-27 13:40:56 +0000

    Update javascripts/main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,13 +4,20 @@ $(function() {
   , gists = {}
   , teams = {};
 
-  $.ajax({
-    url: "https://api.github.com/orgs/workplacesystems/public_members",
-    context: document.body
-  }).done(function(data) { 
-    //$(this).addClass("done");
-    console.log(data);
-  });
+//  $.ajax({
+//    url: "https://api.github.com/orgs/workplacesystems/public_members",
+//    context: document.body
+//  }).done(function(data) { 
+//    //$(this).addClass("done");
+//    console.log(data);
+//  });
+  
+  
+  var jqxhr = $.ajax( "https://api.github.com/orgs/workplacesystems/public_members" )
+    .done(function() { alert("success"); })
+    .fail(function() { alert("error"); })
+    .always(function() { alert("complete"); });
+
 
 });
 
