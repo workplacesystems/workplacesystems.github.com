f7ab341218ea3f7a22b22ab528a5b723c34a3196
garys-workplace <garys@wpmail.com>
2012-11-27 14:00:49 +0000

    Update javascripts/main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,22 +4,31 @@ $(function() {
   , gists = {}
   , teams = {};
 
-  var jqxhr = $.ajax({
-    url: "https://api.github.com/orgs/workplacesystems/members",
-    dataType: "json"
+//  var jqxhr = $.ajax({
+//    url: "https://api.github.com/orgs/workplacesystems/members",
+//    dataType: "json"
 //    context: document.body
-  }).done(function(data) { 
+//  }).done(function(data) { 
     //$(this).addClass("done");
-    console.log(data);
-  }).fail(function() {
-    console.log('error');
-  });
+//    console.log(data);
+//  }).fail(function() {
+//    console.log('error');
+//  });
   
   
 //  var jqxhr = $.ajax( "https://api.github.com/orgs/workplacesystems/members" )
 //    .done(function() { alert("success"); })
 //    .fail(function() { alert("error"); })
 //    .always(function() { alert("complete"); });
+
+
+$.ajax({
+  url: 'https://api.github.com/orgs/workplacesystems/members',
+  success: function(data) {
+    console.log(data);
+    alert('Load was performed.');
+  }
+});
 
 
 });
